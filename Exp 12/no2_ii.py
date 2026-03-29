ce9751912433e54d7a2384c7f95bd7c2fe0a5c08
sibasishsahu68 <24cseaiml009.sibasishsahu@giet.edu>
2026-03-29 13:32:22 +0530

    Delete Exp 12 directory

--- Exp 12/no2_ii.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-categories = ['A','B','C','D']
-values = [3,7,8,5]
-plt.bar(categories,values,color='g',label="Bar Data")
-plt.title("Bar Plot")
-plt.xlabel("X-Axis")
-plt.ylabel("Y-Axis")
-plt.legend()
-plt.show()
